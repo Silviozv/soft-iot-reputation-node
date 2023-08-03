2429dd2ea6746dacfe9013c8cd3849de66357b6f
AllanCapistrano <allan.capistrano3@gmail.com>
2023-08-03 11:10:42 -0300

    feat: create Conduct abstract class

---
+++ src/main/java/br/uefs/larsid/iot/soft/models/conducts/Conduct.java
@@ -0,0 +1,49 @@
+package br.uefs.larsid.iot.soft.models.conducts;
+
+import br.uefs.larsid.iot.soft.enums.ConductType;
+import java.util.logging.Logger;
+
+public abstract class Conduct {
+
+  private ConductType conductType;
+  private static final Logger logger = Logger.getLogger(
+    Conduct.class.getName()
+  );
+
+  public Conduct() {}
+
+  /**
+   * Avalia o serviço que foi prestado pelo dispositivo, de acordo com o tipo de
+   * comportamento do nó.
+   */
+  public void evaluateDevice() {
+    switch (this.conductType) {
+      case HONEST:
+        logger.info("Provided the service.");
+        break;
+      case MALICIOUS:
+        logger.info("Did not provide the service.");
+        break;
+      case SELFISH:
+        logger.info("TODO");
+        break;
+      case CORRUPT:
+        logger.info("TODO");
+        break;
+      case DISTURBING:
+        logger.info("TODO");
+        break;
+      default:
+        logger.severe("Error! ConductType not found.");
+        break;
+    }
+  }
+
+  public ConductType getConductType() {
+    return conductType;
+  }
+
+  public void setConductType(ConductType conductType) {
+    this.conductType = conductType;
+  }
+}
