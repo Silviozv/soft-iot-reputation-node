4b9b57502ba104b86fbd086173b1e68ba72ad50b
AllanCapistrano <allan.capistrano3@gmail.com>
2023-08-03 15:50:47 -0300

    chore: creating node and using mqtt client

--- src/main/java/br/uefs/larsid/iot/soft/models/NodeType.java
+++ src/main/java/br/uefs/larsid/iot/soft/models/NodeType.java
@@ -1,10 +1,18 @@
 package br.uefs.larsid.iot.soft.models;
 
+import br.uefs.larsid.iot.soft.models.conducts.Conduct;
+import br.uefs.larsid.iot.soft.models.conducts.Honest;
+import br.uefs.larsid.iot.soft.models.conducts.Malicious;
 import br.uefs.larsid.iot.soft.services.NodeTypeService;
+import br.uefs.larsid.iot.soft.utils.MQTTClient;
 import java.util.logging.Logger;
 
 public class NodeType implements NodeTypeService {
 
+  private MQTTClient MQTTClient;
+  private int nodeType;
+  private float honestyRate;
+  private Conduct node;
   private static final Logger logger = Logger.getLogger(
     NodeType.class.getName()
   );
@@ -12,10 +20,60 @@ public class NodeType implements NodeTypeService {
   public NodeType() {}
 
   public void start() {
-    logger.info("Iniciando o bundle");
+    // TODO: Adicioanr os demais tipos de nós.
+    switch (nodeType) {
+      case 1:
+        node = new Honest();
+        break;
+      case 2:
+        node = new Malicious(honestyRate);
+        break;
+      default:
+        logger.severe("Error. No node type for this option.");
+        this.stop();
+        break;
+    }
+
+    this.MQTTClient.connect();
+
+    // Apenas para testes.
+    this.node.evaluateDevice();
   }
 
   public void stop() {
-    logger.info("Finalizando o bundle");
+    this.MQTTClient.disconnect();
+    // TODO: Desinscrever dos tópicos
+  }
+
+  public MQTTClient getMQTTClient() {
+    return MQTTClient;
+  }
+
+  public void setMQTTClient(MQTTClient mQTTClient) {
+    MQTTClient = mQTTClient;
+  }
+
+  public int getNodeType() {
+    return nodeType;
+  }
+
+  public void setNodeType(int nodeType) {
+    this.nodeType = nodeType;
+  }
+
+  public float getHonestyRate() {
+    return honestyRate;
+  }
+
+  public void setHonestyRate(float honestyRate) {
+    this.honestyRate = honestyRate;
+  }
+
+  public Conduct getNode() {
+    return node;
+  }
+
+  public void setNode(Conduct node) {
+    this.node = node;
   }
 }
