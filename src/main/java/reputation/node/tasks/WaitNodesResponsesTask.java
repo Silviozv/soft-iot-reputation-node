45663cc42b9bc312ee1cdf293f3a770785ff2373
AllanCapistrano <allan.capistrano3@gmail.com>
2023-11-13 15:22:28 -0300

    feat: create WaitNodesResponsesTask

---
+++ src/main/java/reputation/node/tasks/WaitNodesResponsesTask.java
@@ -0,0 +1,52 @@
+package reputation.node.tasks;
+
+import java.util.TimerTask;
+import java.util.logging.Logger;
+import reputation.node.models.Node;
+
+/**
+ * Classe responsável por determinar um tempo limite para um nó receber as
+ * respostas dos outros nós à solicitação de serviços.
+ *
+ * @author Allan Capistrano
+ * @version 1.0.0
+ */
+public class WaitNodesResponsesTask extends TimerTask {
+
+  private int timer, timeoutWaitNodesResponses;
+  private final Node node;
+  private static final Logger logger = Logger.getLogger(
+    WaitNodesResponsesTask.class.getName()
+  );
+
+  /**
+   * Método construtor
+   *
+   * @param timeoutWaitNodesResponses int - Tempo máximo para aguardar a
+   * resposta dos nós.
+   * @param node Node - Nó o qual está esperando a resposta.
+   */
+  public WaitNodesResponsesTask(int timeoutWaitNodesResponses, Node node) {
+    this.timeoutWaitNodesResponses = timeoutWaitNodesResponses;
+    this.node = node;
+    this.timer = 0;
+  }
+
+  @Override
+  public void run() {
+    this.timer++;
+
+    if ((timer * 1000) >= this.timeoutWaitNodesResponses) {
+      logger.info("Timeout waiting for Nodes responses.");
+
+      /**
+       * Impede o nó de receber as respostas dos outros nós.
+       */
+      this.node.setCanReceiveNodesResponse(false);
+
+      // TODO: Adicionar a chamada da função para verificar a reputaçãos dos nós que responderam e estão na lista
+
+      this.cancel();
+    }
+  }
+}
