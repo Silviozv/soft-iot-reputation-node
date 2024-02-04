8c93ef8dbc91e733a6434ed0e2374f53eee8cd3d
AllanCapistrano <allan.capistrano3@gmail.com>
2024-02-04 11:28:07 -0300

    style: formatting code

--- src/main/java/reputation/node/reputation/IReputationCalc.java
+++ src/main/java/reputation/node/reputation/IReputationCalc.java
@@ -1,19 +1,17 @@
 package reputation.node.reputation;
 
+import dlt.client.tangle.hornet.model.transactions.Transaction;
 import java.util.List;
 
-import dlt.client.tangle.hornet.model.transactions.Transaction;
-
 /**
- * 
+ *
  * @author Allan Capistrano
  * @version 1.0.0
  */
 public interface IReputationCalc {
-
   /**
    * Calcula a reputação de uma coisa.
-   * 
+   *
    * @param evaluationTransactions List<Transaction> - Lista com as transações
    * de avaliação da coisa.
    * @return Double
